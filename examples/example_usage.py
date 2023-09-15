8b8f3d61a2f9ba8a186116744b5842067ab2dfe7
arunking <45511418+arunking@users.noreply.github.com>
2023-09-16 01:14:55 +0530

    Update example_usage.py

--- examples/example_usage.py
+++ examples/example_usage.py
@@ -1,16 +1,20 @@
 # example_usage.py
 
-from paper_folding import PaperFolding
+from fold2moon import PaperFolding
 
-moon_distance_km = 384400
+moon_distance = 384400
+unit = "km"
+
 
 def main():
     # Create an instance of the PaperFolding class
     paper_folding = PaperFolding()
 
     # Example usage
-    result = paper_folding.calculate_paper_height(moon_distance_km, "km")
-    print(result)
+    response = paper_folding.calculate_folds_required(moon_distance, unit)
+    print("To reach a height of {}{}, you'll need to fold the paper {} times and will reach a height of {}{}"
+          .format(moon_distance, unit, response["folds"], response["result"]["height"], response["result"]["unit"]))
+
 
 if __name__ == "__main__":
     main()
